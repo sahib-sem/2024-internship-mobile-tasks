ff702517c38495986d4c5681089d25dcc6045a9c
sahib-sem <semahegnsahib@gmail.com>
2024-03-13 09:35:36 +0300

    created the folder structure

---
+++ on-boarding/product-2/bin/product_2.dart
@@ -0,0 +1,5 @@
+import 'package:product_2/product_2.dart' as product_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${product_2.calculate()}!');
+}
---
+++ on-boarding/product-2/test/product_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:product_2/product_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
