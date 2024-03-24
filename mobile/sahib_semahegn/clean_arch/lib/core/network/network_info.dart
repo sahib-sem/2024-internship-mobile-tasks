27d6b9f1f08431d3e33887c509b0a6a505b51f59
sahib-sem <semahegnsahib@gmail.com>
2024-03-24 16:50:42 +0300

    contracts for the repo and its dependancies

---
+++ mobile/sahib_semahegn/clean_arch/lib/core/network/network_info.dart
@@ -0,0 +1,14 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  InternetConnectionChecker checker;
+
+  NetworkInfoImpl(this.checker);
+
+  @override
+  Future<bool> get isConnected => checker.hasConnection;
+}
--- mobile/sahib_semahegn/clean_arch/lib/features/product/domain/repositories/product_repository.dart
+++ mobile/sahib_semahegn/clean_arch/lib/features/product/domain/repositories/product_repository.dart
@@ -1,11 +1,12 @@
 import 'package:clean_arch/core/error/failure.dart';
+import 'package:clean_arch/features/product/data/models/product_model.dart';
 import 'package:clean_arch/features/product/domain/entities/product.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class ProductRepository {
   Future<Either<Failure, Product>> getProduct(String productId);
   Future<Either<Failure, List<Product>>> getProducts();
-  Future<Either<Failure, void>> CreateProductUsecase(Product product);
-  Future<Either<Failure, void>> UpdateProductUsecase(Product product);
+  Future<Either<Failure, void>> CreateProductUsecase(ProductModel product);
+  Future<Either<Failure, void>> UpdateProductUsecase(ProductModel product);
   Future<Either<Failure, void>> DeleteProductUsecase(String productId);
 }
